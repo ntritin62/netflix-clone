4870b9f14c285b41a81325f89f28fb069e239106
ntritin62 <21522677@gm.uit.edu.vn>
2023-08-24 12:27:25 +0700

    fix bug

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,17 +1,19 @@
-import Firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
-// import { seedDatabase } from "../seed";
+import Firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+// import { seedDatabase } from '../seed';
 
 const config = {
-    apiKey: "AIzaSyBrG91f_btV0ONUfppzcLaNTFO6CYP_1KY",
-    authDomain: "netflix-f04de.firebaseapp.com",
-    projectId: "netflix-f04de",
-    storageBucket: "netflix-f04de.appspot.com",
-    messagingSenderId: "1007213367523",
-    appId: "1:1007213367523:web:6437e582a906f9d28bf5d6",
+  apiKey: 'AIzaSyB7j_xdINgV9dThOLxkjZHkhGVSVo8XJ6s',
+  authDomain: 'netflix-clone-2-d4b41.firebaseapp.com',
+  projectId: 'netflix-clone-2-d4b41',
+  storageBucket: 'netflix-clone-2-d4b41.appspot.com',
+  messagingSenderId: '737072260992',
+  appId: '1:737072260992:web:864be504d4195d39060365',
 };
 
 const firebase = Firebase.initializeApp(config);
+
+// seedDatabase(firebase);
 
 export { firebase };
