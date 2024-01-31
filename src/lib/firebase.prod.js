8292f2b002036ffb309c07aca713622bc01be22e
ntritin62 <21522677@gm.uit.edu.vn>
2024-01-31 21:00:00 +0700

    update new firebase config

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,19 +1,19 @@
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-// import { seedDatabase } from '../seed';
+import { seedDatabase } from '../seed';
 
 const config = {
-  apiKey: 'AIzaSyB7j_xdINgV9dThOLxkjZHkhGVSVo8XJ6s',
-  authDomain: 'netflix-clone-2-d4b41.firebaseapp.com',
-  projectId: 'netflix-clone-2-d4b41',
-  storageBucket: 'netflix-clone-2-d4b41.appspot.com',
-  messagingSenderId: '737072260992',
-  appId: '1:737072260992:web:864be504d4195d39060365',
+  apiKey: 'AIzaSyBM3HZc-dyNdZbO-cV54S0x5H4I5GQ5Ckc',
+  authDomain: 'netflix-1174c.firebaseapp.com',
+  projectId: 'netflix-1174c',
+  storageBucket: 'netflix-1174c.appspot.com',
+  messagingSenderId: '915436786573',
+  appId: '1:915436786573:web:5f767e335e615275fea3c4',
 };
 
 const firebase = Firebase.initializeApp(config);
 
-// seedDatabase(firebase);
+seedDatabase(firebase);
 
 export { firebase };
